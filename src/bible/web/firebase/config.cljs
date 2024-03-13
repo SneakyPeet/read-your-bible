94cf446e943f9fd3d944d83a309246866a02a399
Pieter Koornhof <koornhof.pieter@gmail.com>
2024-03-13 19:52:03 +0200

    analytics

--- src/bible/web/firebase/config.cljs
+++ src/bible/web/firebase/config.cljs
@@ -1,10 +1,18 @@
 (ns bible.web.firebase.config
   (:require [shadow.resource :as rc]
             ["firebase/app" :as firebase-app]
-            ["firebase/firestore" :as firestore]))
+            ["firebase/firestore" :as firestore]
+            ["firebase/analytics" :as analytics]))
 
 (def firebase-config  (js/JSON.parse (rc/inline "config.json")))
 
 (def app (firebase-app/initializeApp firebase-config))
 
 (def firestore (firestore/getFirestore app))
+
+
+(goog-define use-analytics? false)
+
+(def analytics
+  (when use-analytics?
+    (analytics/getAnalytics app)))
